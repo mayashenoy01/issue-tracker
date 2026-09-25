fe9dbab518db8b5ceeb646dd3459b2386f7ff480
Maya Shenoy <m2shenoy@uwaterloo.ca>
2026-09-24 21:20:07 -0400

    add user model test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,19 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "user cannot be saved without a name and password" do
+    user = User.new
+
+    assert_not user.save
+    assert_includes user.errors[:name], "can't be blank"
+    assert_includes user.errors[:password], "can't be blank"
+  end
+
+  test "user can be saved with a name and password" do
+    user = User.new(name: "Maya", password: "password")
+
+    assert user.save
+    assert user.password_digest.present?
+  end
 end
